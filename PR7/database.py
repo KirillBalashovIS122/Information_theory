5223c6be243e6a7f4d4a9fd5768bf688becbc05d
Kirill Bakashov <kira.kimi2003@mail.ru>
2024-04-19 00:11:48 +0300

    added docstring modules, as well as fixed the error of output of all users

--- PR7/database.py
+++ PR7/database.py
@@ -1,3 +1,26 @@
+"""
+Модуль для управления базой данных пользователей.
+
+Этот модуль предоставляет функции для создания базы данных,
+добавления, удаления и изменения пользователей,
+а также для хеширования паролей.
+
+Функции:
+- create_database(): Создает базу данных и необходимые таблицы (User_types, Users).
+- hash_password(password): Хеширует пароль методом MD5.
+- get_all_users(): Возвращает список всех пользователей.
+- add_user(login, password, utype_id): Добавляет нового пользователя в базу данных.
+- get_user_by_login(login): Возвращает пользователя по логину.
+- delete_user_by_login(login): Удаляет пользователя из базы данных по логину.
+- change_user_password(login, new_password): Изменяет пароль пользователя.
+
+Константы:
+- DB_PATH: Путь к файлу базы данных.
+
+Примечание:
+Внимание! В целях безопасности реальные пароли пользователей не должны храниться в открытом виде.
+Они хранятся в базе данных в виде хэш-суммы (MD5).
+"""
 import os
 import sqlite3
 import hashlib
@@ -5,6 +28,12 @@ import hashlib
 DB_PATH = 'base2.db'
 
 def create_database():
+    """
+    Создает базу данных и необходимые таблицы.
+
+    Если база данных уже существует, она будет удалена и создана заново.
+    Таблицы User_types и Users будут созданы, если их не существует.
+    """
     if os.path.exists(DB_PATH):
         os.remove(DB_PATH)
 
@@ -38,23 +67,50 @@ def create_database():
             connection.close()
 
 def hash_password(password):
+    """
+    Хеширует пароль методом MD5.
+
+    Args:
+    - password: Пароль для хеширования.
+
+    Returns:
+    - Хэш-сумма пароля в виде строки.
+    """
     hash_md5 = hashlib.md5()
     hash_md5.update(password.encode())
     return hash_md5.hexdigest()
+
 def get_all_users():
+    """
+    Возвращает список всех пользователей.
+
+    Returns:
+    - Список всех пользователей в виде списка кортежей.
+    """
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
     cursor.execute("SELECT login FROM Users")
-    all_user=cursor.fetchone()
+    all_users = cursor.fetchall()
     cursor.close()
-    return all_user
+    return all_users
+
+
 def add_user(login, password, utype_id):
+    """
+    Добавляет нового пользователя в базу данных.
+
+    Args:
+    - login: Логин нового пользователя.
+    - password: Пароль нового пользователя.
+    - utype_id: Идентификатор типа пользователя (1 для администратора, 2 для пользователя).
+    """
     hashed_password = hash_password(password)
     try:
         connection = sqlite3.connect(DB_PATH)
         cursor = connection.cursor()
 
-        cursor.execute("INSERT INTO Users (login, password, utype_id) VALUES (?,?,?);", (login, hashed_password, utype_id))
+        cursor.execute("INSERT INTO Users (login, password, utype_id) VALUES (?,?,?);",
+                    (login, hashed_password, utype_id))
         connection.commit()
         cursor.close()
         print("Пользователь добавлен в базу данных")
@@ -65,6 +121,15 @@ def add_user(login, password, utype_id):
             connection.close()
 
 def get_user_by_login(login):
+    """
+    Возвращает пользователя по логину.
+
+    Args:
+    - login: Логин пользователя.
+
+    Returns:
+    - Кортеж с информацией о пользователе или None, если пользователь не найден.
+    """
     try:
         connection = sqlite3.connect(DB_PATH)
         cursor = connection.cursor()
@@ -80,7 +145,14 @@ def get_user_by_login(login):
     finally:
         if connection:
             connection.close()
+
 def delete_user_by_login(login):
+    """
+    Удаляет пользователя из базы данных по логину.
+
+    Args:
+    - login: Логин пользователя.
+    """
     try:
         connection = sqlite3.connect(DB_PATH)
         cursor = connection.cursor()
@@ -96,6 +168,13 @@ def delete_user_by_login(login):
             connection.close()
 
 def change_user_password(login, new_password):
+    """
+    Изменяет пароль пользователя.
+
+    Args:
+    - login: Логин пользователя.
+    - new_password: Новый пароль.
+    """
     try:
         connection = sqlite3.connect(DB_PATH)
         cursor = connection.cursor()
